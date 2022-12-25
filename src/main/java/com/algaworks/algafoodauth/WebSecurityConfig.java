6fe14be424797c80381a0622bf2c7f098b4c55a5
mnishimori <mnishimori@yahoo.com.br>
2022-12-25 18:48:49 -0300

    feat: 22.9. Configurando o fluxo Authorization Server com Password Credentials e Opaque Tokens

---
+++ src/main/java/com/algaworks/algafoodauth/WebSecurityConfig.java
@@ -0,0 +1,39 @@
+package com.algaworks.algafoodauth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication()
+                .withUser("akio")
+                .password(this.getPasswordEncoder().encode("123456"))
+                .roles("ADMIN")
+                .and()
+                .withUser("joao")
+                .password(this.getPasswordEncoder().encode("123456"))
+                .roles("ADMIN");
+    }
+
+    @Bean
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
